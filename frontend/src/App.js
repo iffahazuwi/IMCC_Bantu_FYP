dc7c41e96403e256ffb8c1ad2aeae0a67baa5d9b
iffahazuwi <iffahazuwi@gmail.com>
2024-04-01 22:52:45 +0800

    create initial webpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import ArticleList from './components/ArticleList';
+import Form from './components/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [articles, setArticles] = useState([])
+    const [editedArticle, setEditedArticle] = useState(null)
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/get', {
+            'method': 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(resp => resp.json())
+            .then(resp => setArticles(resp))
+            .catch(error => console.log(error))
+    }, [])
+
+    const editArticle = (article) => {
+        setEditedArticle(article)
+    }
+
+    const updatedData = (article) => {
+        const new_article = articles.map(my_article => {
+            if (my_article.id == article.id) {
+                return article
+            } else {
+                return my_article
+            }
+        })
+        setArticles(new_article)
+    }
+
+    const openForm = () => {
+        setEditedArticle({ title: '', body: '' })
+    }
+
+    const insertedArticle = (article) => {
+        const new_articles = [...articles, article]
+        setArticles(new_articles)
+    }
+
+    const deleteArticle = (article) => {
+        const new_articles = articles.filter(myarticle => {
+            if (myarticle.id === article.id) {
+                return false;
+            }
+            return true
+        })
+        setArticles(new_articles)
+    }
+
+    return (
+        <div className="App">
+            <div className='row'>
+                <div className='col'>
+                    <h1>Flask and ReactJS Course</h1>
+                </div>
+                <div className='col'>
+                    <button
+                        className='btn btn-success'
+                        onClick={openForm}
+                    >Insert Article</button>
+                </div>
+            </div>
+
+
+            <ArticleList articles={articles} editArticle={editArticle} deleteArticle={deleteArticle} />
+
+            {editedArticle ? <Form article={editedArticle} updatedData={updatedData} insertedArticle={insertedArticle} /> : null}
+        </div>
+    );
 }
 
 export default App;
