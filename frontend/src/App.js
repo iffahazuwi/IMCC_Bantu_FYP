90810551d531e89d12d360beed2e1a66b4883203
iffahazuwi <iffahazuwi@gmail.com>
2024-04-02 17:21:25 +0800

    create navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,79 +1,40 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import ArticleList from './components/ArticleList';
-import Form from './components/Form';
+import Navigation from './Navigation';
+import Home from './pages/Home';
+import MatchingPage from './pages/MatchingPage';
+import CommunityPage from './pages/CommunityPage';
+import UserPage from './pages/UserPage'
+import {Route, Routes} from "react-router-dom"
 
 function App() {
-
-    const [articles, setArticles] = useState([])
-    const [editedArticle, setEditedArticle] = useState(null)
-
-    useEffect(() => {
-        fetch('http://127.0.0.1:5000/get', {
-            'method': 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(resp => resp.json())
-            .then(resp => setArticles(resp))
-            .catch(error => console.log(error))
-    }, [])
-
-    const editArticle = (article) => {
-        setEditedArticle(article)
-    }
-
-    const updatedData = (article) => {
-        const new_article = articles.map(my_article => {
-            if (my_article.id == article.id) {
-                return article
-            } else {
-                return my_article
-            }
-        })
-        setArticles(new_article)
-    }
-
-    const openForm = () => {
-        setEditedArticle({ title: '', body: '' })
-    }
-
-    const insertedArticle = (article) => {
-        const new_articles = [...articles, article]
-        setArticles(new_articles)
-    }
-
-    const deleteArticle = (article) => {
-        const new_articles = articles.filter(myarticle => {
-            if (myarticle.id === article.id) {
-                return false;
-            }
-            return true
-        })
-        setArticles(new_articles)
-    }
-
+    // let Component
+    // switch (window.location.pathname) {
+    //     case "/":
+    //         Component = <Home />
+    //         break
+    //     case "/matching-page":
+    //         Component = <MatchingPage />
+    //         break
+    //     case "/community-page":
+    //         Component = <CommunityPage />
+    //         break
+    //     case "/user-page":
+    //         Component = <UserPage />
+    //         break
+    // }
     return (
-        <div className="App">
-            <div className='row'>
-                <div className='col'>
-                    <h1>Flask and ReactJS Course</h1>
-                </div>
-                <div className='col'>
-                    <button
-                        className='btn btn-success'
-                        onClick={openForm}
-                    >Insert Article</button>
-                </div>
+        <>
+            <Navigation />
+            <div className='container'>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/matching-page" element={<MatchingPage />} />
+                    <Route path="/community-page" element={<CommunityPage />} />
+                    <Route path="/user-page" element={<UserPage />} />
+                </Routes>
             </div>
-
-
-            <ArticleList articles={articles} editArticle={editArticle} deleteArticle={deleteArticle} />
-
-            {editedArticle ? <Form article={editedArticle} updatedData={updatedData} insertedArticle={insertedArticle} /> : null}
-        </div>
-    );
+        </>
+    )
 }
 
 export default App;
---
+++ frontend/src/pages/UserPage.js
@@ -0,0 +1,3 @@
+export default function Home(){
+    return <h1>User Page</h1>
+}
