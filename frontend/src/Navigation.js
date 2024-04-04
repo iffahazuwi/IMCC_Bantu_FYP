93a0a5b41aedf5de75f1e996f4db544dfc8ed29a
iffahazuwi <iffahazuwi@gmail.com>
2024-04-04 18:56:00 +0800

    add usm logo, add button in user page

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -4,6 +4,7 @@ export default function Navigation() {
     return (
         <nav className="nav">
             <Link to="/home" className="site-title">
+                <img src="./USM_Logo.png" alt="" width={36} height={36}/>
                 <img src="./Latest_IMCC-Logo-2017.png" alt="" width={36} height={36}/>
                 IMCC Bantu
             </Link>
--- frontend/src/UserPage.js
+++ frontend/src/UserPage.js
@@ -1,7 +1,25 @@
+import './App.css';
+
 export default function UserPage(){
     return (
-        <div className='col mb-3' align='center'>
-            <h1>User Page</h1>
+        <div className="App">
+            <div className='col mb-3' align='center'>
+                <h1>User Page</h1>
+            </div>
+            <div className='col'>
+                <div className="mb-4" align='right'>
+                    <button
+                        className='btn btn-primary'
+                        // onClick={openForm}
+                    >Notification</button>
+                </div>
+                <div className="mb-2" align='center'>
+                    <button
+                        className='btn btn-success'
+                        // onClick={openForm}
+                    >Mentor Application Form</button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
